90a5a3c17f96789d648acdc89b45ce6d76e15f21
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2024-02-03 21:17:04 +0900

    add migrations

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -29,6 +29,7 @@ public class SeedData
             ReleaseDate = DateTime.Parse("1989-2-12"),
             Genre = "Romantic Comedy",
             Price = 7.99M,
+            Rating = "R"
           },
 
           new Movie
@@ -37,6 +38,7 @@ public class SeedData
             ReleaseDate = DateTime.Parse("1984-3-13"),
             Genre = "Comedy",
             Price = 8.99M,
+            Rating = "CH",
           },
 
           new Movie
@@ -45,6 +47,7 @@ public class SeedData
             ReleaseDate = DateTime.Parse("1986-2-23"),
             Genre = "Comedy",
             Price = 9.99M,
+            Rating = "G",
           },
 
           new Movie
@@ -53,6 +56,7 @@ public class SeedData
             ReleaseDate = DateTime.Parse("1959-4-15"),
             Genre = "Western",
             Price = 3.99M,
+            Rating = "NA",
           }
       );
       context.SaveChanges();
